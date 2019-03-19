510dce1484a690bd0b62c7a39d8cf2225a4dc149
Ahmad Ismail <ahmadismail@ahmads-macbook-pro.local>
2019-03-20 00:18:53 +0200

    Initial Commit

---
+++ Popular on NYT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Popular on NYT
+//
+//  Created by Ahmad Ismail on 3/20/19.
+//  Copyright © 2019 Ahmad Ismail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
