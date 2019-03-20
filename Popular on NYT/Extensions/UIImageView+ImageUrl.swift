c9a94a17992f33509ca2ef3fe8776d4c20c0eff8
Ahmad Ismail <ahmadismail@ahmads-macbook-pro.local>
2019-03-20 17:45:32 +0200

    Added Article Table View Cell

---
+++ Popular on NYT/Extensions/UIImageView+ImageUrl.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+ImageUrl.swift
+//  Popular on NYT
+//
+//  Created by Ahmad Ismail on 3/20/19.
+//  Copyright © 2019 Ahmad Ismail. All rights reserved.
+//
+
+import Kingfisher
+
+extension UIImageView {
+    
+    func setup(withImageUrl urlString: String?) {
+        if let urlString = urlString, let url = URL(string: urlString) {
+            let imageResource = ImageResource(downloadURL: url)
+            self.kf.setImage(with: imageResource, placeholder: nil, options:  [.transition(.fade(0.3))])
+        } else {
+            self.image = nil
+        }
+    }
+}
+
---
+++ Popular on NYT/View/ArticleCell.swift
@@ -0,0 +1,39 @@
+//
+//  ArticleCell.swift
+//  Popular on NYT
+//
+//  Created by Ahmad Ismail on 3/20/19.
+//  Copyright © 2019 Ahmad Ismail. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bylineLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    
+    var articleViewModel: ArticleViewModel! {
+        didSet {
+            titleLabel.text = articleViewModel.title
+            bylineLabel.text = articleViewModel.byline
+            dateLabel.text = articleViewModel.publishedDate
+            thumbnailImageView.setup(withImageUrl: articleViewModel.thumbnailUrlString)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
