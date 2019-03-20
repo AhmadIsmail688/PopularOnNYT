ed9214d790098edc364e57e46e5a735f3ce8919a
Ahmad Ismail <ahmadismail@ahmads-macbook-pro.local>
2019-03-20 16:51:12 +0200

    Unit testing Article View Model

--- Popular on NYTTests/Popular_on_NYTTests.swift
+++ Popular on NYTTests/Popular_on_NYTTests.swift
@@ -12,23 +12,48 @@ import XCTest
 class Popular_on_NYTTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func constructTestArticle() -> Article {
+        let thumbnailMetaData = ArticleMediaMetaData(
+            url: "https://thumbnail.image.jpg",
+            format: "Standard Thumbnail"
+        )
+        let mediumImageMetaData = ArticleMediaMetaData(
+            url: "https://medium.image.jpg",
+            format: "mediumThreeByTwo440"
+        )
+        let articleMedia = ArticleMedia(
+            mediaMetadata: [thumbnailMetaData,mediumImageMetaData]
+        )
+        let article = Article(
+            title: "Article Title",
+            byline: "by the author",
+            abstract: "Article Abstract",
+            publishedDate: "2019-03-20",
+            media: [articleMedia]
+        )
+        return article
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testArticleViewModel() {
+       
+        let testArticle = constructTestArticle()
+        let articleViewModel = ArticleViewModel(article: testArticle)
+       
+        XCTAssertEqual(testArticle.title, articleViewModel.title)
+        XCTAssertEqual(testArticle.byline, articleViewModel.byline)
+        XCTAssertEqual(testArticle.publishedDate, articleViewModel.publishedDate)
+        
+        let mediaMetaData = testArticle.media.first?.mediaMetadata
+        let thumbnailUrlString = mediaMetaData?.filter{
+            $0.format == "Standard Thumbnail"
+            }.first?.url
+        XCTAssertEqual(thumbnailUrlString, articleViewModel.thumbnailUrlString)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
