b4fdb892da6d0c8bce3cb36e071b20ba6b8b4879
Ahmad Ismail <ahmadismail@ahmads-macbook-pro.local>
2019-03-20 16:28:09 +0200

    Added Article View Model

---
+++ Popular on NYT/View Model/ArticleViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleViewModel.swift
+//  Popular on NYT
+//
+//  Created by Ahmad Ismail on 3/20/19.
+//  Copyright © 2019 Ahmad Ismail. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleViewModel {
+    
+    let title: String
+    let byline: String
+    let publishedDate: String
+    let thumbnailUrlString: String?
+    
+    init(article: Article) {
+        
+        self.title = article.title
+        self.byline = article.byline
+        self.publishedDate = article.publishedDate
+        
+        let mediaMetaData = article.media.first?.mediaMetadata
+        self.thumbnailUrlString = mediaMetaData?.filter{
+            $0.format == "Standard Thumbnail"
+            }.first?.url
+    }
+    
+}
